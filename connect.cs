66885aee5b27e02d86900f8943226280880c3799
RC_Student_lab <RC_Student_lab@labG9AEB3>
2024-10-24 04:33:54 +0000

    Add project files.

---
+++ connect.cs
@@ -0,0 +1,13 @@
+﻿namespace part_1
+{
+    public class connect
+    {
+
+        public string connecting()
+        {
+
+            //then return connection
+            return "Data Source=(localdb)\\claiming_system;Initial Catalog=claim_system;";
+        }
+    }
+}
